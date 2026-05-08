cdf9523722ac73e7b267aa033ab0e6824f48f275
William de Aguiar Campos <138343439+GDCom62@users.noreply.github.com>
2026-05-07 21:02:12 -0300

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-exit on error
-set -o errexit
-
-pip install -r requirements.txt
-
-# Criar as tabelas no banco de dados do servidor
-python -c "from app import app, db; with app.app_context(): db.create_all()"
