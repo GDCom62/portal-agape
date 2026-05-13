a4c6cf76dcaece6d2d3f22df3cbc95c22cc93c26
William de Aguiar Campos <138343439+GDCom62@users.noreply.github.com>
2026-05-13 14:14:10 -0300

    Update Streamlit command to use app.py

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 gunicorn -k gevent -w 1 --bind 0.0.0.0:8000 main:app &
 
 # 2. Inicia o Streamlit na porta oficial pública fornecida pelo Railway ($PORT)
-streamlit run seu_arquivo_portal.py --server.port $PORT --server.address 0.0.0.0
+streamlit run app.py --server.port $PORT --server.address 0.0.0.0
