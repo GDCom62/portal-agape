587b7dbb57fc386b4fbcc5f7bafbcb46e17c6645
William de Aguiar Campos <138343439+GDCom62@users.noreply.github.com>
2026-05-13 15:20:42 -0300

    Update comments in start.sh for clarity

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 1. Inicia o Gunicorn na porta interna 8000 em segundo plano (&)
+# 1. Inicia sua API/Chat na porta interna 8000 (em segundo plano)
 gunicorn -k gevent -w 1 --bind 0.0.0.0:8000 main:app &
 
-# 2. Inicia o Streamlit na porta oficial pública fornecida pelo Railway ($PORT)
+# 2. Inicia o Portal Streamlit na porta pública obrigatória do Railway ($PORT)
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
